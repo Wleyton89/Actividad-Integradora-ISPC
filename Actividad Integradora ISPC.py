c6bbba80aae4d0e32e73393f2a41fc5d6c37e6ed
Alejandro Leyton <leytonale96@hotmail.com>
2022-07-05 23:55:45 -0300

    On branch master
     Initial commit
    
     Changes to be committed:
    	new file:   Actividad Integradora ISPC.py

---
+++ Actividad Integradora ISPC.py
@@ -0,0 +1,52 @@
+print("Este programa cuenta con 4 funciones para calcular al ingresar 5 numeros")
+print("Opcion 1 - Sumar \nOpcion 2 - Sacar promedio\nOpcion 3 - Sacar el Maximo de un numero \nOpcion 4 - Sacar el minimo de un numero")
+opcion = int(input("Ingrese la opcion que desea utilizar (1,2,3 o 4): "))
+if opcion == 1:
+    print("Usted eligio la Opcion 1 - Sumar")
+    list = []
+    result = 0
+    suma = 0
+    for i in range(5):
+        num = int(input("Ingrese un numero"))
+        list.append(num)
+    for number in list:
+        result += number
+    print(f"El resultado de la suma es: {result}")
+if opcion == 2:
+    print("Usted eligio la Opcion 2 - Sacar promedio")
+    list = []
+    result = 0
+    for i in range(5):
+        num = int(input("Ingrese un numero: "))
+        list.append(num)
+    for number in list:
+        suma += number
+        promedio = suma / len(list)
+    print(f"El promedio de los numeros ingresados es: {promedio}")
+if opcion == 3:
+    print("Usted eligio la Opcion 3 - Sacar el Maximo de un numero")
+    list = []
+    result = 0
+    for i in range(5):
+        num = int(input("Ingrese un numero: "))
+        list.append(num)
+    for number in list:
+        result = max(list)
+    print(f"El valor maximo de los numeros ingresados es: {result}")
+if opcion == 4:
+    print("Usted eligio la Opcion 4 - Sacar el minimo de un numero")
+    list = []
+    result = 0
+    for i in range(5):
+        num = int(input("Ingrese un numero: "))
+        list.append(num)
+    for number in list:
+        result = min(list)
+    print(f"El valor minimo de los numeros ingresados es: {result}")
+
+print("Gracias por utilizar el programa!")
+print("Participantes:\n-Alejandro Antonio Leyton Segovia\n-William Bernardo Leyton Segovia ")
+
+
+
+
