c254d3e371f911560f67303d57c05bceed612c8e
Wleyton89 <108815579+Wleyton89@users.noreply.github.com>
2022-08-16 20:22:42 -0300

    Update Actividad Integradora ISPC.py

--- Actividad Integradora ISPC.py
+++ Actividad Integradora ISPC.py
@@ -1,12 +1,12 @@
-print("Este programa cuenta con 4 funciones para calcular al ingresar 5 numeros")
-print("Opcion 1 - Sumar \nOpcion 2 - Sacar promedio\nOpcion 3 - Sacar el Maximo de un numero \nOpcion 4 - Sacar el minimo de un numero")
-opcion = int(input("Ingrese la opcion que desea utilizar (1,2,3 o 4): "))
+print("Este programa cuenta con 4 funciones para calcular al ingresar 5 números")
+print("Opción 1 - Sumar \nOpcion 2 - Sacar promedio\nOpcion 3 - Sacar el Maximo de un numero \nOpcion 4 - Sacar el minimo de un número")
+opcion = int(input("Ingrese la Opción que desea utilizar (1,2,3 o 4): "))
 def sumar():
     list = []
     result = 0
     suma = 0
     for i in range(5):
-        num = int(input("Ingrese un numero: "))
+        num = int(input("Ingrese un número: "))
         list.append(num)
     for number in list:
         result += number
@@ -15,21 +15,21 @@ def promedio():
     list = []
     result = 0
     for i in range(5):
-        num = int(input("Ingrese un numero: "))
+        num = int(input("Ingrese un número: "))
         list.append(num)
     for number in list:
         suma += number
         promedio = suma / len(list)
-    print(f"El promedio de los numeros ingresados es: {promedio}")
+    print(f"El promedio de los números ingresados es: {promedio}")
 def maximo():
     list = []
     result = 0
     for i in range(5):
-        num = int(input("Ingrese un numero: "))
+        num = int(input("Ingrese un número: "))
         list.append(num)
     for number in list:
         result = max(list)
-    print(f"El valor maximo de los numeros ingresados es: {result}")
+    print(f"El valor máximo de los números ingresados es: {result}")
 def minimo():
     list = []
     result = 0
@@ -40,16 +40,16 @@ def minimo():
         result = min(list)
     print(f"El valor minimo de los numeros ingresados es: {result}")
 if opcion == 1:
-    print("Usted eligio la Opcion 1 - Sumar")
+    print("Usted eligió la Opción 1 - Sumar")
     sumar()
 if opcion == 2:
-    print("Usted eligio la Opcion 2 - Sacar promedio")
+    print("Usted eligió la Opción 2 - Sacar promedio")
     promedio()
 if opcion == 3:
-    print("Usted eligio la Opcion 3 - Sacar el Maximo de un numero")
+    print("Usted eligió la Opción 3 - Sacar el Maximo de un numero")
     maximo()
 if opcion == 4:
-    print("Usted eligio la Opcion 4 - Sacar el minimo de un numero")
+    print("Usted eligió la Opción 4 - Sacar el minimo de un numero")
     minimo()
 
 print("Gracias por utilizar el programa!")
